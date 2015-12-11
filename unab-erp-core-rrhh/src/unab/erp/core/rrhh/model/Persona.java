5284e7c0bf76519a8d42bc8e732b4332a6252ce4
Miguel Castillo <mcastillo@cajmetro.cl>
2015-12-11 16:03:54 -0400

    se modifican  id de objeto y de agrega relación con dirrecciones

--- unab-erp-core-rrhh/src/unab/erp/core/rrhh/model/Persona.java
+++ unab-erp-core-rrhh/src/unab/erp/core/rrhh/model/Persona.java
@@ -5,53 +5,65 @@
  */
 package unab.erp.core.rrhh.model;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author miguelcastillo
  */
 public class Persona {
     
-    
-    
-    public int    Id;
+    public int    Persona_id;
     public String Nombre;
     public String Apellido_pat;
     public String Apellido_mat;
     public int    Rut;
+    public Set<Direccion> direcciones = new HashSet();
+    
+    public void setDirecciones(Set<Direccion> direcciones) {
+        this.direcciones = direcciones;
+    }
 
-  
-
+    public Set<Direccion> getDirecciones() {
+        return this.direcciones;
+    }
+    
     public Persona(){
     
     }
     
-    public int getId() {
-        return Id;
-    }
+
 
     public String getNombre() {
-        return Nombre;
+        return this.Nombre;
     }
 
     public String getApellido_pat() {
-        return Apellido_pat;
+        return this.Apellido_pat;
     }
 
     public String getApellido_mat() {
-        return Apellido_mat;
+        return this.Apellido_mat;
     }
 
     public int getRut() {
-        return Rut;
+        return this.Rut;
     }
 
     public String getDv() {
-        return Dv;
+        return this.Dv;
     }
 
-    public void setId(int Id) {
-        this.Id = Id;
+    public int getPersona_id() {
+        return this.Persona_id;
     }
+
+    public void setPersona_id(int Persona_id) {
+        this.Persona_id = Persona_id;
+    }
+
 
     public void setNombre(String Nombre) {
         this.Nombre = Nombre;
